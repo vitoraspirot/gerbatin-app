dddfccca49bdc2ec85b23c2d8e446c38fa692146
maria eduarda <eduardabastos.99@hotmail.com>
2025-08-25 22:00:47 -0300

    altera texto

--- src/screens/category-list/category-list.screen.tsx
+++ src/screens/category-list/category-list.screen.tsx
@@ -6,7 +6,7 @@ import { styles } from "./category-list.styles";
 export const CategoryListScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Lista por categorias</Text>
+      <Text>Lista</Text>
     </View>
   );
 };
