c27091c14b3bb638c9988d804c5e821cfc8129f0
Jordanamcampos <matoscjordana@gmail.com>
2025-08-28 17:01:29 -0300

    imagens dos drones

--- src/screens/product-details/product-details.screen.tsx
+++ src/screens/product-details/product-details.screen.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import { View, Text } from "react-native";
 import { styles } from "./product-detais.styles";
 
+
 export const ProductDetaisScreen = () => {
   return (
     <View style={styles.container}>
       <Text>Detalhes do produto</Text>
+     
     </View>
   );
 };
